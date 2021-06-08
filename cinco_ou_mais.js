5027dc98a498e82a668851dddd3fb9390d0bf8cd
Jéssica Zanchetta <jessica.zanchetta@hotmail.com>
2021-06-08 19:43:02 -0300

    Essa foi fodagit add cinco_ou_mais.js

---
+++ cinco_ou_mais.js
@@ -0,0 +1,29 @@
+function solucao(precos) 
+{
+    precos[100, 500, 100, 200,50]
+    
+    if(precos.length<5){
+    soma=0;  
+    for (i=0;i<precos.length;i++)
+{ 
+    soma=soma+precos[i]     
+}
+    console.log(soma)   //Output0    
+}
+    precos[100, 100, 100, 50]
+    
+    if(precos.length >= 5){
+    function ordenaNum(a,b){
+    return a-b;
+}
+    precos.sort(ordenaNum)
+    precos.shift();
+    soma=0;  
+    for (i=0;i<precos.length;i++)
+{ 
+    soma=soma+precos[i]     
+}
+            console.log(soma)//Output1
+}
+
+}
