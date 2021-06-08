196baa820b2a71b1f29798481ea7c2acdf1c6f65
Jéssica Zanchetta <jessica.zanchetta@hotmail.com>
2021-06-08 19:47:08 -0300

    Black Mirror

---
+++ me_segue_no_insta.js
@@ -0,0 +1,10 @@
+function solucao(largura, altura) {
+    if (altura>largura){
+        console.log('RETRATO')
+    } else if (largura>altura){
+        console.log('PAISAGEM')
+    } else if (largura===altura){
+        console.log('QUADRADA')
+    }
+    
+  }
