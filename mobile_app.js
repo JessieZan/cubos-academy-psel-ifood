b3731805edece6380d5fe59b2f6dda69db3328ca
Jéssica Zanchetta <jessica.zanchetta@hotmail.com>
2021-06-08 19:47:58 -0300

    Only for Celphones

---
+++ mobile_app.js
@@ -0,0 +1,21 @@
+function solucao(tempo, distancia) {
+	if (tempo <= 5)
+    {
+        console.log(600)
+    }
+    else if (tempo >=5 && tempo<=60)
+    {
+        console.log ((tempo*100) + (distancia*50))
+    }
+    else if (tempo>60 )
+    {
+        if (distancia<100)
+    {
+            console.log(distancia*200)
+    } else
+    {
+            console.log(distancia*150)
+    }
+  }
+
+}
